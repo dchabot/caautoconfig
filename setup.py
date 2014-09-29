e8bd0e5b4e01bcb9896e3ddbd33915581f227ff9
Daron Chabot <dchabot@bnl.gov>
2014-09-29 18:01:27 -0400

    Bump version string to 1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='CAAutoConfig',
-      version='1.0',
+      version='1.1',
       description='Python Utilities to autogenerate archiver file',
       author='Stuart Wilkins',
       author_email='swilkins@bnl.gov',
